4e3b809eba344dcf0969cec23f775cfa734079af
sivanpayyadakath <sivan.payyadakath@gmail.com>
2018-11-09 18:42:17 +0900

    some refactoring

--- lunch_team.rb
+++ lunch_team.rb
@@ -1,58 +1,55 @@
 team_number = 3
 TEAMS = []
+SQUAD = []          #final valid groups
 
-members =
-  [
-      { "name"=>"やまぐち", "japanese"=>"true" },
-      { "name"=>"はすみ", "japanese"=>"true" },
-      { "name"=>"みとも", "japanese"=>"true" },
-      { "name"=>"タイン", "japanese"=>"false" },
-      { "name"=>"ポール", "japanese"=>" false" },
-      { "name"=>"ハー", "japanese"=>"false" },
-      { "name"=>"シヴァン","japanese"=>"false" },
-      { "name"=>"おしだ", "japanese"=>"true" },
-      { "name"=>"えび", "japanese"=>"true" }
-  ]
+#input array of hash
 
-def make_group(team_number,members)
+members = [
+    { "name" => "やまぐち", "japanese" => "true" },
+    { "name" => "はすみ", "japanese" => "true" },
+    { "name" => "みとも", "japanese" => "true" },
+    { "name" => "タイン", "japanese" => "false" },
+    { "name" => "ポール", "japanese" => " false" },
+    { "name" => "ハー", "japanese" => "false" },
+    { "name" => "シヴァン","japanese" => "false" },
+    { "name" => "おしだ", "japanese" => "true" },
+    { "name" => "えび", "japanese" => "true" }
+]
+
+def make_group(team_number,members)             #random groups
   members.each do |a|
     TEAMS << a["name"]
   end
-  TEAMS.permutation(team_number).to_a
-
+  TEAMS.combination(team_number).to_a
 end
 
-def lunch_team(groups,members)
-  groups.each do |ar|
-    flag=1
-    k=[]
-    ar.each do |a|
-      members.each do |m|
-        if a == m["name"]
-          if m["japanese"] == "true"
+def print_list(array_of_arrays)                 #print list
+  array_of_arrays.each do |a|
+    p a
+  end
+end
+
+def lunch_team(groups,members)                  #finding valid squad
+  groups.each do |group|
+    flag = 1
+    k = []
+    group.each do |person|
+      members.each do |member|
+        if person == member["name"]
+          if member["japanese"] == "true"
             flag = 0
           end
         end
       end
-      unless flag != 0
-        k << a
-        p k if k.size == 3
-      end
+      k << person
+    end
+    if flag == 0 && k.size == 3
+      SQUAD << k
     end
   end
 end
 
 
-
-
 groups = make_group(team_number,members)
 lunch_team(groups,members)
-
-
-
-
-
-
-
-
-
+print_list(SQUAD)
